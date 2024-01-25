c2c816530c0f5dfd5732504790d916e72a7b3b00
baptistejouin <baptiste.jouin44@gmail.com>
2024-01-25 19:24:35 +0100

    fix dark theme

--- src/services/prefersColorScheme.ts
+++
@@ -1,9 +0,0 @@
-export function setup() {
-	const html = document.querySelector("html");
-	if (
-		localStorage.getItem("theme") === "dark" ||
-		(!(localStorage.getItem("theme")) &&
-			window.matchMedia("(prefers-color-scheme: dark)").matches)
-	)
-		html.classList.add("dark");
-}
